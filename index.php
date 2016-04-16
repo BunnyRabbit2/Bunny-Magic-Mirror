d681712279ac64cf65bd428ada31c4d34bb16bce
lewis storey <lewis storey>
2016-04-16 17:20:14 +0100

    Added a folder so I can add files to be ignored Added html5 stuff to index
    Signed-off-by: lewis storey <lewis storey>

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <title>Magic Mirror Page</title>
     
     <link href="css/magic-mirror.css" rel="stylesheet">
