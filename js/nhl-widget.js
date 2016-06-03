2a29b7d85d7d0f3272f6c6112956dab6a78c2f81
lewis storey <lewis storey>
2016-06-03 20:25:59 +0100

    HOTFIX: Found new bug in NHL widget Would crash if only one date was returned (Stupid Stanley Cup breaking my code) Now fixed hopefully for real this time. I really need to condense the NHL widget down. Too much code repeating
    Signed-off-by: lewis storey <lewis storey>

--- js/nhl-widget.js
+++ js/nhl-widget.js
@@ -2,6 +2,15 @@
 // 19 is STL, 6 is BOS, 28 is SJS
 var teamIDs = [19, 6, 28] // populate this with the correct ids for the teams you want
 
+function addPreZero(numberIn) {
+    if (numberIn < 10) {
+        return "0" + numberIn;
+    }
+    else {
+        return numberIn;
+    }
+}
+
 function populateNhlDiv() {
     var useDebugDates = false;
 
@@ -13,9 +22,12 @@ function populateNhlDiv() {
     dayBeforYesterday.setDate(yesterday.getDate() - 1);
 
     if (useDebugDates) {
-        today = new Date("2016-02-15");
+        /*today = new Date("2016-02-15");
         yesterday = new Date("2016-02-14");
-        dayBeforYesterday = new Date("2016-02-13");
+        dayBeforYesterday = new Date("2016-02-13");*/
+        today = new Date("2016-06-03");
+        yesterday = new Date("2016-06-02");
+        dayBeforYesterday = new Date("2016-06-01");
     }
 
     var startDate = "";
@@ -23,12 +35,12 @@ function populateNhlDiv() {
 
     // 8am is the switch point into the next day. Pretty much guarantees all games are done
     if (today.getHours() > 8) {
-        startDate = yesterday.getFullYear() + "-" + (yesterday.getMonth() + 1) + "-" + yesterday.getDate();
-        endDate = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+        startDate = yesterday.getFullYear() + "-" + addPreZero(yesterday.getMonth() + 1) + "-" + addPreZero(yesterday.getDate());
+        endDate = today.getFullYear() + "-" + addPreZero(today.getMonth() + 1) + "-" + addPreZero(today.getDate());
     }
     else {
-        startDate = dayBeforYesterday.getFullYear() + "-" + (dayBeforYesterday.getMonth() + 1) + "-" + dayBeforYesterday.getDate();
-        endDate = yesterday.getFullYear() + "-" + (yesterday.getMonth() + 1) + "-" + yesterday.getDate();
+        startDate = dayBeforYesterday.getFullYear() + "-" + addPreZero(dayBeforYesterday.getMonth() + 1) + "-" + addPreZero(dayBeforYesterday.getDate());
+        endDate = yesterday.getFullYear() + "-" + addPreZero(yesterday.getMonth() + 1) + "-" + addPreZero(yesterday.getDate());
     }
 
     var JSONString = "https://statsapi.web.nhl.com/api/v1/schedule?startDate=" + startDate + "&endDate=" + endDate;
@@ -39,7 +51,7 @@ function populateNhlDiv() {
         var newStart = new Date(startDate);
         var newEnd = new Date(endDate);
 
-        if (data.dates.length > 0) {
+        if (data.dates.length > 1) {
             // Yesterdays Games
             if (data.dates[0].games.length > 0) {
                 html += '<div class="row"><div class="col-md-12">Games for ' + newStart.getDate() + '/' + (newStart.getMonth() + 1) + '</div></div>';
@@ -98,10 +110,75 @@ function populateNhlDiv() {
                 }
             }
         }
+        else if (data.dates.length == 1) {
+            if (data.dates[0].date == startDate) {
+                if (data.dates[0].games.length > 0) {
+                    html += '<div class="row"><div class="col-md-12">Games for ' + newStart.getDate() + '/' + (newStart.getMonth() + 1) + '</div></div>';
+                    var noGames = true;
+
+                    $.each(data.dates[0].games, function (i, game) {
+                        for (i = 0; i < teamIDs.length; i++) {
+                            if (game.teams.away.team.id == teamIDs[i] || game.teams.home.team.id == teamIDs[i]) {
+                                // We have a match!
+                                html += '<div class="row nhl-game"><div class="col-md-12">';
+
+                                html += '<div class="row"><div class="col-md-12"><h4>' + game.status.detailedState + '</h4></div></div>'
+                                html += '<div class="row"><div class="col-md-4"><p>' + nhlTeamNames[game.teams.away.team.id].abbreviation + '</p></div><div class="col-md-4"><p> @ </p></div><div class="col-md-4"><p>' + nhlTeamNames[game.teams.home.team.id].abbreviation + '</p></div></div>'
+                                html += '<div class="row"><div class="col-md-4"><p>' + game.teams.away.score + '</p></div><div class="col-md-4"><p> - </p></div><div class="col-md-4"><p>' + game.teams.home.score + '</p></div></div>'
+
+                                noGames = false;
+
+                                html += '</div></div>';
+                            }
+                        }
+                    });
+
+                    if (noGames) {
+                        html += '<div class="row nhl-game"><div class="col-md-12"><h4>No Games For Chosen Teams</h4></div></div>';
+                    }
+                }
+
+                html += '<div class="row"><div class="col-md-12">Games for ' + newEnd.getDate() + '/' + (newEnd.getMonth() + 1) + '</div></div>';
+                html += '<div class="row nhl-game"><div class="col-md-12"><h4>No Games For Chosen Teams</h4></div></div>';
+            }
+            else {
+                html += '<div class="row"><div class="col-md-12">Games for ' + newStart.getDate() + '/' + (newStart.getMonth() + 1) + '</div></div>';
+                html += '<div class="row nhl-game"><div class="col-md-12"><h4>No Games For Chosen Teams</h4></div></div>';
+                
+                if (data.dates[0].games.length > 0) {
+                    html += '<div class="row"><div class="col-md-12">Games for ' + newEnd.getDate() + '/' + (newEnd.getMonth() + 1) + '</div></div>';
+
+                    var noGames = true;
+
+                    $.each(data.dates[0].games, function (i, game) {
+                        for (i = 0; i < teamIDs.length; i++) {
+                            if (game.teams.away.team.id == teamIDs[i] || game.teams.home.team.id == teamIDs[i]) {
+                                // We have a match!
+                                html += '<div class="row nhl-game"><div class="col-md-12">';
+
+                                html += '<div class="row"><div class="col-md-12"><h4>' + game.status.detailedState + '</h4></div></div>'
+                                html += '<div class="row"><div class="col-md-4"><p>' + nhlTeamNames[game.teams.away.team.id].abbreviation + '</p></div><div class="col-md-4"><p> @ </p></div><div class="col-md-4"><p>' + nhlTeamNames[game.teams.home.team.id].abbreviation + '</p></div></div>'
+                                if (game.status.statusCode > 1) {
+                                    html += '<div class="row"><div class="col-md-4"><p>' + game.teams.away.score + '</p></div><div class="col-md-4"><p> - </p></div><div class="col-md-4"><p>' + game.teams.home.score + '</p></div></div>'
+                                }
+
+                                noGames = false;
+
+                                html += '</div></div>';
+                            }
+                        }
+                    });
+
+                    if (noGames) {
+                        html += '<div class="row nhl-game"><div class="col-md-12"><h4>No Games For Chosen Teams</h4></div></div>';
+                    }
+                }
+            }
+        }
         else {
             html += '<div class="row"><div class="col-md-12">Games for ' + newStart.getDate() + '/' + (newStart.getMonth() + 1) + '</div></div>';
             html += '<div class="row nhl-game"><div class="col-md-12"><h4>No Games For Chosen Teams</h4></div></div>';
-            
+
             html += '<div class="row"><div class="col-md-12">Games for ' + newEnd.getDate() + '/' + (newEnd.getMonth() + 1) + '</div></div>';
             html += '<div class="row nhl-game"><div class="col-md-12"><h4>No Games For Chosen Teams</h4></div></div>';
         }
